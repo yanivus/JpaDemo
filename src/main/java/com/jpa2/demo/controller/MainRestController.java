2b94761a94a79b7a8e23a46ca1e5fe90dc769857
ssinha <suvinay_sinha@eogresources.com>
2022-04-09 19:04:25 -0500

    Search by email reponse

--- src/main/java/com/jpa2/demo/controller/MainRestController.java
+++ src/main/java/com/jpa2/demo/controller/MainRestController.java
@@ -3,29 +3,34 @@ package com.jpa2.demo.controller;
 import com.jpa2.demo.dto.AuthorStatistic;
 import com.jpa2.demo.entity.Author;
 import com.jpa2.demo.entity.Book;
-import com.jpa2.demo.repository.BaseRepository;
+import com.jpa2.demo.model.EmptyJsonResponse;
 import com.jpa2.demo.repository.jpa.AuthorRepository;
-import com.jpa2.demo.repository.jpa.BaseRepositoryImpl;
 import com.jpa2.demo.repository.jpa.BookRepository;
-import com.jpa2.demo.repository.jpa.BookRepositoryImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class MainRestController {
 
-    @Autowired
     AuthorRepository authorRepository;
-
-    @Autowired
     BookRepository bookRepository;
 
+    @Autowired
+    public void setAuthorRepository(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @Autowired
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
 
     @RequestMapping(path="/authors", method = RequestMethod.GET)
-    @ResponseBody
     public List<Author> showAuthors() {
         return authorRepository.findAll();
     }
@@ -36,10 +41,17 @@ public class MainRestController {
         return authorRepository.findAuthorStatistic();
     }
 
+    @RequestMapping(path="/authorByEmail", method = RequestMethod.GET)
+    public ResponseEntity<Optional<Author>> fetchAuthorByEmail(@RequestParam("email") String email) {
+        Optional<Author> author = authorRepository.findByEmailIgnoreCase(email);
+        if (author.isPresent()) {
+            return new ResponseEntity<>(authorRepository.findByEmailIgnoreCase(email), HttpStatus.OK);
+        }
+        return new ResponseEntity(new EmptyJsonResponse(), HttpStatus.OK);
+    }
 
 
     @RequestMapping(path="/books", method = RequestMethod.GET)
-    @ResponseBody
     public List<Book> showBooks() {
         return bookRepository.findAll();
     }
---
+++ src/main/java/com/jpa2/demo/model/EmptyJsonResponse.java
@@ -0,0 +1,7 @@
+package com.jpa2.demo.model;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+
+@JsonSerialize
+public class EmptyJsonResponse {
+}
